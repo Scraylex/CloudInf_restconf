97d5f26144384b979c9e9ab9be34d3044a9cc8d2
Dominic Gabriel <domi94@gmx.ch>
2019-09-23 17:03:55 +0200

    Added device config and first edit of the script

--- restconf_example.py
+++ restconf_example.py
@@ -3,6 +3,7 @@ from typing import List
 
 import requests
 import yaml
+from jinja2 import Environment, FileSystemLoader
 
 import restconf_helpers
 
@@ -17,9 +18,15 @@ def load_devices() -> List[dict]:
         return hosts
 
 
+def load_device_config():
+    with open('config.yaml', 'r') as config_file:
+        config = yaml.load(config_file.read(), Loader=yaml.FullLoader)
+        return config
+
+
 def init_logger():
     _logger = logging.getLogger('restconf')
-    _logger.setLevel(logging.DEBUG)
+    #_logger.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
@@ -38,11 +45,40 @@ def print_interfaces(host: dict) -> None:
     print(get_interfaces(host=host))
 
 
+def configure_interface():
+    #load config from yaml
+    #config = {'is_loopback': 'True'}
+    config = load_device_config()
+
+    for k, v in config.items():
+        if k == 'interfaces':
+            # render and configure interfaces
+            print(k)
+            print(v)
+            for int in v:
+                env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+                template = env.get_template('interface.j2')
+                print(template.render(int))
+        elif k == 'bgp':
+            print("configure bpg")
+        elif k == 'ospf':
+            print('configure ospf')
+
+
+def configure_ospf():
+    pass
+
+
+def configure_loopback():
+    pass
+
+
 def main():
     devices = load_devices()
     for device in devices:
         logger.info(f'Getting information for device {device}')
         print_interfaces(host=device)
+    configure_interface()
 
 
 if __name__ == '__main__':
